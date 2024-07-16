fbe7eea8ee666a0b426f7b34a40c1b9193d18885
MohitChaudhari <mohitchoudharu2@gmail.com>
2024-07-16 17:53:48 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@mui/lab/Rating']
+      external: ['@mui/lab/Rating','@popperjs/core']
     }
   }
 })
